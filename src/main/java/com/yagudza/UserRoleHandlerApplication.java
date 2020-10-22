5fc4f4ebb3540de271f5129484912d7271d11c30
Alex <allgud@mail.ru>
2020-10-22 13:49:51 +0300

    initialization, bd created

---
+++ src/main/java/com/yagudza/UserRoleHandlerApplication.java
@@ -0,0 +1,12 @@
+package com.yagudza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRoleHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRoleHandlerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/yagudza/domain/Roles.java
@@ -0,0 +1,31 @@
+package com.yagudza.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
